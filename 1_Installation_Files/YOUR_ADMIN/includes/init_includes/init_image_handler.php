839b3ae9a06b5da38529862dd486f78757afe6ec
Crystal Jones <divawebdzgn@aol.com>
2015-04-08 05:59:19 -0700

    Update version number

--- 1_Installation_Files/YOUR_ADMIN/includes/init_includes/init_image_handler.php
+++ 1_Installation_Files/YOUR_ADMIN/includes/init_includes/init_image_handler.php
@@ -16,7 +16,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     //$uninstall = 'uninstall';
 
     // set version
-    $version = '4.3.3';
+    $version = '4.3.4';
 
     // flags
     $install_incomplete = false;
