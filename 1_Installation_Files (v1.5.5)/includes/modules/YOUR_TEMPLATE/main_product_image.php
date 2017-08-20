c60818514e4f6b4f0eda8fd5f8cb560a152e314f
lat9 <lat9@vinosdefrutastropicales.com>
2017-08-20 08:41:21 -0400

    #22: Apply PSR-2 styling
    ... to the 1.5.5e version.

--- 1_Installation_Files (v1.5.5)/includes/modules/YOUR_TEMPLATE/main_product_image.php
+++ 1_Installation_Files (v1.5.5)/includes/modules/YOUR_TEMPLATE/main_product_image.php
@@ -9,7 +9,7 @@
  * @version $Id: main_product_image.php 4663 2006-10-02 04:08:32Z drbyte $
  */
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
 $products_image_extension = substr($products_image, strrpos($products_image, '.'));
 $products_image_base = str_replace($products_image_extension, '', $products_image);
@@ -18,19 +18,19 @@ $products_image_large = $products_image_base . IMAGE_SUFFIX_LARGE . $products_im
 
 // check for a medium image else use small
 if (!file_exists(DIR_WS_IMAGES . 'medium/' . $products_image_medium)) {
-  $products_image_medium = DIR_WS_IMAGES . $products_image;
+    $products_image_medium = DIR_WS_IMAGES . $products_image;
 } else {
-  $products_image_medium = DIR_WS_IMAGES . 'medium/' . $products_image_medium;
+    $products_image_medium = DIR_WS_IMAGES . 'medium/' . $products_image_medium;
 }
 // check for a large image else use medium else use small
 if (!file_exists(DIR_WS_IMAGES . 'large/' . $products_image_large)) {
-  if (!file_exists(DIR_WS_IMAGES . 'medium/' . $products_image_medium)) {
-    $products_image_large = DIR_WS_IMAGES . $products_image;
-  } else {
-    $products_image_large = DIR_WS_IMAGES . 'medium/' . $products_image_medium;
-  }
+    if (!file_exists(DIR_WS_IMAGES . 'medium/' . $products_image_medium)) {
+        $products_image_large = DIR_WS_IMAGES . $products_image;
+    } else {
+        $products_image_large = DIR_WS_IMAGES . 'medium/' . $products_image_medium;
+    }
 } else {
-  $products_image_large = DIR_WS_IMAGES . 'large/' . $products_image_large;
+    $products_image_large = DIR_WS_IMAGES . 'large/' . $products_image_large;
 }
 /*
 echo
@@ -39,5 +39,3 @@ echo
 'Large ' . $products_image_large . '<br><br>';
 */
 // to be built into a single variable string
-
-?>
