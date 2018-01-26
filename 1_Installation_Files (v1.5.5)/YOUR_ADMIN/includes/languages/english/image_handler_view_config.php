93a897af06e38a3b6ed17fff88764895e78acba4
lat9 <lat9@vinosdefrutastropicales.com>
2018-01-26 08:43:49 -0500

    Remove extraneous 's'.

--- 1_Installation_Files (v1.5.5)/YOUR_ADMIN/includes/languages/english/image_handler_view_config.php
+++ 1_Installation_Files (v1.5.5)/YOUR_ADMIN/includes/languages/english/image_handler_view_config.php
@@ -4,7 +4,7 @@
 // Copyright (c) 2018 Vinos de Frutas Tropicales
 //
 define('HEADING_TITLE', 'View Image Handler<sup>5</sup> Configuration');
-define('INSTRUCTIONS', 'This page displays the various configuration values used by the admin- and storefront-processing for <em>Image Handler</em><sup>5</sup>.  Those values are defined either in your store\'s <em>Configuration</em> menu (the left-hand table) or are defined in the files <code>%1$ss</code> and <code>%2$s</code> (the right-hand table).');
+define('INSTRUCTIONS', 'This page displays the various configuration values used by the admin- and storefront-processing for <em>Image Handler</em><sup>5</sup>.  Those values are defined either in your store\'s <em>Configuration</em> menu (the left-hand table) or are defined in the files <code>%1$s</code> and <code>%2$s</code> (the right-hand table).');
 
 define('CONFIG_HEADING', 'Values from <a href="%1$s">Configuration / Images</a>.  Click the link in the first column to edit the associated value.');
 
