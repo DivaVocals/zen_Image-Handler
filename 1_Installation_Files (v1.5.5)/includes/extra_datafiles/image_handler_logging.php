9c9e997aa1cae990475a05202b3fc84b1f62eb0d
lat9 <lat9@vinosdefrutastropicales.com>
2018-05-19 16:49:04 -0400

    #90: Provide easier debug-enable
    Include a script through which the store owner can enable/disable Image
    Handler's debug/trace feature.

---
+++ 1_Installation_Files (v1.5.5)/includes/extra_datafiles/image_handler_logging.php
@@ -0,0 +1,11 @@
+<?php
+// -----
+// Part of the Image Handler-5 plugin, v5.0.1 and later.  Provided by Cindy Merkin (lat9)
+// Copyright (C) 2018, Vinos de Frutas Tropicales.
+//
+if (!defined('IH_DEBUG_ADMIN')) {
+    define('IH_DEBUG_ADMIN', 'false');
+}
+if (!defined('IH_DEBUG_STOREFRONT')) {
+    define('IH_DEBUG_STOREFRONT', 'false');
+}
